b866eb0253be19d06a5588c1b0cbb7d973b2b41a
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-06-19 11:55:34 +0400

    'g2'

---
+++ DesignLayoutPractice/app/src/main/java/com/example/designlayoutpractice/ui/grid2Fragment.java
@@ -0,0 +1,18 @@
+package com.example.designlayoutpractice.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.designlayoutpractice.R;
+
+public class grid2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.grid2, container, false);
+    }
+}
