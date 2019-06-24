395eb419faeeedaaaa560ce45bbfbb189b28e306
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-06-24 11:57:49 +0400

    'half'

---
+++ linier2/app/src/main/java/com/example/linier2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.linier2;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
