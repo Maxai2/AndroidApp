a19ba465b7d3f6643ef43cde964b562b78af1626
Хамелеон <maxali946@gmail.com>
2019-07-02 18:09:36 +0400

    'frame'

--- Minesweeper/app/src/main/java/com/example/minesweeper/FieldLvl.java
+++ Minesweeper/app/src/main/java/com/example/minesweeper/FieldLvl.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -73,25 +74,28 @@ public class FieldLvl extends AppCompatActivity {
         for (int i = 0; i < row; ++i) {
             for (int j = 0; j < col; ++j) {
 
-                ImageButton ib = new ImageButton(this);
-                minesField.addView(ib);
+                FrameLayout fl = new FrameLayout(this);
 
-                //GridLayout.LayoutParams param = new GridLayout.LayoutParams(GridLayout.spec(i,1, 1f), GridLayout.spec(j,1, 1f));
-                GridLayout.LayoutParams param = (GridLayout.LayoutParams)ib.getLayoutParams();
-                param.width = 0;
-                param.height = 0;
-                param.columnSpec = GridLayout.spec(i,1,1f);
-                param.rowSpec = GridLayout.spec(j,1,1f);
-//                param.setGravity(Gravity.FILL);
-                param.setMargins(0, 0, 0, 0);
-                ib.setLayoutParams(param);
-                ib.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        check(v);
-                    }
-                });
-                ib.setBackgroundResource(R.drawable.flag);
+
+//                ImageButton ib = new ImageButton(this);
+//                minesField.addView(ib);
+//
+//                //GridLayout.LayoutParams param = new GridLayout.LayoutParams(GridLayout.spec(i,1, 1f), GridLayout.spec(j,1, 1f));
+//                GridLayout.LayoutParams param = (GridLayout.LayoutParams)ib.getLayoutParams();
+//                param.width = 0;
+//                param.height = 0;
+//                param.columnSpec = GridLayout.spec(i,1,1f);
+//                param.rowSpec = GridLayout.spec(j,1,1f);
+////                param.setGravity(Gravity.FILL);
+//                param.setMargins(0, 0, 0, 0);
+//                ib.setLayoutParams(param);
+//                ib.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        check(v);
+//                    }
+//                });
+//                ib.setBackgroundResource(R.drawable.flag);
 
             }
         }
