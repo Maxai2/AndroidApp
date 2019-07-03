9ae29167987ee7ed2864e01cd25e34a843f669f4
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-07-03 12:13:17 +0400

    'style'

--- Minesweeper/app/src/main/java/com/example/minesweeper/FieldLvl.java
+++ Minesweeper/app/src/main/java/com/example/minesweeper/FieldLvl.java
@@ -1,15 +1,21 @@
 package com.example.minesweeper;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.CollapsibleActionView;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FieldLvl extends AppCompatActivity {
 
@@ -32,6 +38,8 @@ public class FieldLvl extends AppCompatActivity {
         timesLeft.setText("000");
 
         minesField = (GridLayout)findViewById(R.id.mineField);
+
+        getSupportActionBar().hide();
 
         Intent intent = getIntent();
 
@@ -76,11 +84,38 @@ public class FieldLvl extends AppCompatActivity {
 
                 FrameLayout fl = new FrameLayout(this);
 
+                Button b = new Button(this, null, R.style.Widget_AppCompat_Button_Borderless);
+                fl.addView(b);
+
+                FrameLayout.LayoutParams paramB = (FrameLayout.LayoutParams)b.getLayoutParams();
+                paramB.height = paramB.width = FrameLayout.LayoutParams.MATCH_PARENT;
+                paramB.setMargins(1, 1, 1, 1);
+                b.setLayoutParams(paramB);
+
+                b.setBackgroundColor(Color.rgb(0, 0, 0));
+                b.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        check(v);
+                    }
+                });
+
+                minesField.addView(fl);
+                GridLayout.LayoutParams paramFl = (GridLayout.LayoutParams)fl.getLayoutParams();
+
+                paramFl.width = 0;
+                paramFl.height = 0;
+                paramFl.columnSpec = GridLayout.spec(i,1,1f);
+                paramFl.rowSpec = GridLayout.spec(j,1,1f);
+
+                fl.setLayoutParams(paramFl);
+                fl.setBackgroundColor(Color.rgb(255, 0, 0));
+                fl.setPadding(0, 0, 0, 0);
 
 //                ImageButton ib = new ImageButton(this);
 //                minesField.addView(ib);
 //
-//                //GridLayout.LayoutParams param = new GridLayout.LayoutParams(GridLayout.spec(i,1, 1f), GridLayout.spec(j,1, 1f));
+//                //GridLayout.LayoutParams param = new GridLayout.LayoutParams(GridLayout.spec(i, 1, 1f), GridLayout.spec(j, 1, 1f));
 //                GridLayout.LayoutParams param = (GridLayout.LayoutParams)ib.getLayoutParams();
 //                param.width = 0;
 //                param.height = 0;
@@ -102,5 +137,6 @@ public class FieldLvl extends AppCompatActivity {
     }
 
     private void check(View v) {
+
     }
 }
