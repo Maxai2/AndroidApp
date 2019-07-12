2b350dee14b80fa02785d0f65829068214979f2f
Хамелеон <maxali946@gmail.com>
2019-07-12 13:19:27 +0400

    'numbersImg'

--- 15Numbers/app/src/main/java/com/example/a15numbers/MainActivity.java
+++ 15Numbers/app/src/main/java/com/example/a15numbers/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.a15numbers;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridLayout field;
+    TextView timeLeft;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        field = findViewById(R.id.field);
+        timeLeft = findViewById(R.id.timeLeft);
+        timeLeft.setText("00:00");
+
+        final Handler handler = new Handler();
+        Timer timer = new Timer(false);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        timeLeft.setText(String.valueOf(count++));
+                    }
+                });
+            }
+        };
+
+        timer.scheduleAtFixedRate(timerTask, 1000, 1000);
+
+        gridField();
+    }
+
+    void gridField() {
+
+    }
+
+    public void reset(View view) {
+
     }
 }
