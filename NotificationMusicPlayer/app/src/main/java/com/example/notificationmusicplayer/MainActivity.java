740a5c9faa8d082a2dd989a985f0f90a5f7d4c40
Хамелеон <maxali946@gmail.com>
2019-07-16 20:01:04 +0400

    'btns'

--- NotificationMusicPlayer/app/src/main/java/com/example/notificationmusicplayer/MainActivity.java
+++ NotificationMusicPlayer/app/src/main/java/com/example/notificationmusicplayer/MainActivity.java
@@ -3,21 +3,14 @@ package com.example.notificationmusicplayer;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int notificationId = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +21,12 @@ public class MainActivity extends AppCompatActivity {
     public void open(View view) {
 
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder;
+        Notification builder;
 
         Context context = getApplicationContext();
+
+        RemoteViews notiflay = new RemoteViews(getPackageName(), R.layout.notification_m_p);
+        RemoteViews notiflaySmall = new RemoteViews(getPackageName(), R.layout.notification_m_p_small);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
 
@@ -44,36 +40,39 @@ public class MainActivity extends AppCompatActivity {
 
             manager.createNotificationChannel(channel);
 
-            builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+            builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.vinil)
+                    .setStyle(null)
+                    .setCustomContentView(notiflaySmall)
+                    .setCustomBigContentView(notiflay)
+                    .build();
         } else {
-            builder = new NotificationCompat.Builder(context);
+            builder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.vinil)
+                    .setStyle(null)
+                    .setCustomContentView(notiflaySmall)
+                    .setCustomBigContentView(notiflay)
+                    .build();
         }
 
-        PendingIntent action = PendingIntent.getActivity(context,
-                0, new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/Prosto_Amiran")),
-                PendingIntent.FLAG_CANCEL_CURRENT); // Flag indicating that if the described PendingIntent already exists, the current one should be canceled before generating a new one.
+//        PendingIntent action = PendingIntent.getActivity(context,
+//                0, new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/Prosto_Amiran")),
+//                PendingIntent.FLAG_CANCEL_CURRENT);
 
-//        PendingIntent action = null;
+        manager.notify(101, builder);
+    }
 
-        builder.setContentTitle("Notification Title")
-                .setContentText("Notification Content")
-                .setContentIntent(action)
-                .setPriority(Notification.PRIORITY_HIGH) // приоритет для старых версий андроида
-//                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.vinil))
-                // http://stackoverflow.com/questions/35647821/android-notification-addaction-deprecated-in-api-23
-                .addAction(R.drawable.prev_music, "", action)
-                .addAction(R.drawable.vinil, "2", action/*2*/)
-                .addAction(R.drawable.vinil, "3", action/*3*/)
-                .setColor(Color.rgb(0, 0, 0))
-                .setAutoCancel(true)
-                .setSmallIcon(R.drawable.vinil);
-
-        //Notification notification = new NotificationCompat.BigTextStyle(builder)
-        //        .bigText(bigText).build();
-
-        Notification notification = new NotificationCompat.BigPictureStyle(builder)
-                .bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.vinil)).build();
-
-        manager.notify(101, notification);
+    public void notifAct(View view) {
+        switch (view.getId()) {
+            case R.id.prev:
+                break;
+            case R.id.playPause:
+                view.setBackground(getResources().getDrawable(R.drawable.pause_music));
+                break;
+            case R.id.next:
+                break;
+            case R.id.favUnfav:
+                break;
+        }
     }
 }
