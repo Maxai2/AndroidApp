5a186370fb07cff2e951b8f8258a24be0717d1a7
Хамелеон <maxali946@gmail.com>
2019-07-18 19:12:36 +0400

    'st'

--- PapaJohnsApp/app/src/main/java/com/example/papajohnsapp/MainActivity.java
+++ PapaJohnsApp/app/src/main/java/com/example/papajohnsapp/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.papajohnsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.mikepenz.actionitembadge.library.ActionItemBadge;
+import com.mikepenz.actionitembadge.library.ActionItemBadgeAdder;
 import com.mikepenz.iconics.typeface.IIcon;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-//    TextView basketCount;
-//    int basketCountNum;
+    TextView basketCount;
+    int basketCountNum;
 
     int[] categImg = new int[] {
             R.drawable.pizza, R.drawable.pasta
@@ -63,46 +67,46 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
 
-        if (badgeCount > 0) {
-            Drawable d  = getDrawable(R.drawable.basket);
+//        if (badgeCount > 0) {
+//            Drawable d  = getDrawable(R.drawable.basket);
+//
+//            ActionItemBadge.update(this, menu.findItem(R.id.basket),d, ActionItemBadge.BadgeStyles.BLUE,badgeCount);
+//        } else {
+//            ActionItemBadge.hide(menu.findItem(R.id.basket));
+//        }
 
-            ActionItemBadge.update(this, menu.findItem(R.id.basket),d, ActionItemBadge.BadgeStyles.BLUE,badgeCount);
-        } else {
-            ActionItemBadge.hide(menu.findItem(R.id.basket));
-        }
+        new ActionItemBadgeAdder().act(this).menu(menu).title(R.string.sample_2).itemDetails(0, SAMPLE2_ID, 1).showAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS).add(bigStyle, 1);
+        return super.onCreateOptionsMenu(menu);
+        final MenuItem menuItem = menu.findItem(R.id.basket);
 
-//        new ActionItemBadgeAdder().act(this).menu(menu).title(R.string.sample_2).itemDetails(0, SAMPLE2_ID, 1).showAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS).add(bigStyle, 1);
-//        return super.onCreateOptionsMenu(menu);
-//        final MenuItem menuItem = menu.findItem(R.id.basket);
-//
-//        View actionView = MenuItemCompat.getActionView(menuItem);
-//        basketCount = (TextView) actionView.findViewById(R.id.cart_badge);
-//
-//        setupBadge();
-//
-//        actionView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                onOptionsItemSelected(menuItem);
-//            }
-//        });
+        View actionView = MenuItemCompat.getActionView(menuItem);
+        basketCount = (TextView) actionView.findViewById(R.id.cart_badge);
+
+        setupBadge();
+
+        actionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOptionsItemSelected(menuItem);
+            }
+        });
 
         return true;
     }
 
-//    private void setupBadge() {
-//
-//        if (basketCount != null) {
-//            if (basketCountNum == 0) {
-//                if (basketCount.getVisibility() != View.GONE) {
-//                    basketCount.setVisibility(View.GONE);
-//                }
-//            } else {
-//                basketCount.setText(String.valueOf(Math.min(basketCountNum, 99)));
-//                if (basketCount.getVisibility() != View.VISIBLE) {
-//                    basketCount.setVisibility(View.VISIBLE);
-//                }
-//            }
-//        }
-//    }
+    private void setupBadge() {
+
+        if (basketCount != null) {
+            if (basketCountNum == 0) {
+                if (basketCount.getVisibility() != View.GONE) {
+                    basketCount.setVisibility(View.GONE);
+                }
+            } else {
+                basketCount.setText(String.valueOf(Math.min(basketCountNum, 99)));
+                if (basketCount.getVisibility() != View.VISIBLE) {
+                    basketCount.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    }
 }
