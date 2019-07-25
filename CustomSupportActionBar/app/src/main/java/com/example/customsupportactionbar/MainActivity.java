6e60de0e88dc7a7ee9fc823c001bf609c6f4592b
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-07-25 11:32:26 +0400

    'actBarDone'

--- CustomSupportActionBar/app/src/main/java/com/example/customsupportactionbar/MainActivity.java
+++ CustomSupportActionBar/app/src/main/java/com/example/customsupportactionbar/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.customsupportactionbar;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        //getSupportActionBar().setElevation(0);
+//        View view = getSupportActionBar().getCustomView();
+//        TextView name = view.findViewById(R.id.name);
+//
+//        name.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "You have clicked tittle", Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 }
