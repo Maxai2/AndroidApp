97f4ee4cf011eac1a4e3a8acee5d7d1bbfb707d3
Ali Mahmudov <maxali946@gmail.com>
2019-08-28 15:56:36 +0400

    'linkAnim'

--- GoodBadPersonAnimation/app/src/main/java/com/example/goodbadpersonanimation/MainActivity.java
+++ GoodBadPersonAnimation/app/src/main/java/com/example/goodbadpersonanimation/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
+//http://www.edu4java.com/en/androidgame/androidgame1.html
+
 public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
