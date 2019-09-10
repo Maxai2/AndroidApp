69abf46a26ba75d972143d698712588051fad62d
Ali Mahmudov <maxali946@gmail.com>
2019-09-10 18:10:53 +0400

    'smth'

--- ReciverTraciever/TrancieverReciver/app/src/main/java/com/example/myapplication/MainActivity.java
+++ ReciverTraciever/TrancieverReciver/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    public static final String MY_MESSAGE = "com.exmaple.ReciverTranciver";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        editText = findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
